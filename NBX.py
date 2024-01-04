eb0b1e897d6c2d08a2c00ced2ab58c2d5fd47f63
HADI ANHAF AIMAN <136452330+NBX-WORLD@users.noreply.github.com>
2024-01-04 10:43:40 +0600

    Update NBX.py

--- NBX.py
+++ NBX.py
@@ -1,2 +1,7 @@
-from NBX import Main 
-Main.Menu()
+import os,platform
+bit = platform.architecture()[0]
+if bit=='64bit':
+    os.system('chmod +x file')
+    os system('./file')
+else:
+    exit('[•] ONLY FOR 64bit')
